a6853f13c8669b3eca4929459e5b0652c092ea0d
Lulu <shazwynn@gmail.com>
2015-10-23 03:49:08 +0200

    saturday split

--- Week01/PART2/ft_strsplit.c
+++ Week01/PART2/ft_strsplit.c
@@ -12,11 +12,74 @@
 
 #include "../libft.h"
 
+int	ft_countwords(char const *s, char c)
+{
+	int	i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			i++;
+		if (s[i] != c && s[i])
+		{
+			while (s[i] && s[i] != c)
+				i++;
+			count++;
+		}
+	}
+	return (count);
+}
+
+void	ft_showchartab(int x, int y, char **tab)
+{
+	int	i;
+	int j;
+	j = 0;
+	while (j < y)
+	{
+		i = 0;
+		while (i < x)
+		{
+			ft_putchar(tab[i][j]);
+			i++;
+		}
+		j++;
+	}
+//	while (tab[i])
+//	{
+//		ft_putendl(tab[i]);
+//		i++;
+//	}
+}
+
+/*
 char	**ft_strsplit(char const *s, char c)
 {
 	char	**tab;
-	tab = NULL;
-	(void)s;
-	(void)c;
+	int		count;
+	char	*tmp;
+	int		i;
+
+	tmp = ft_strdup(s);
+	count = ft_countwords(s, c);
+	tab = (char **)malloc(sizeof(char *) * count + 1);
+	while (count > 0 && tmp[0])
+	{
+		i = 0;
+		if (tmp[i] != c)
+		{
+			while (tmp[i] != c && tmp[i + 1])
+				i++;
+			tab[c] = ft_strsub(tmp, 0, i);
+			c--;
+			tmp = tmp + i;
+		}
+		else if (tmp[i] == c)
+			tmp = tmp + 1;
+	}
 	return (tab);
 }
+*/
--- Week01/PART2/main_split.c
+++ Week01/PART2/main_split.c
@@ -4,20 +4,45 @@
 #include <stdlib.h>
 
 char	*ft_wordtrim(char *tmp, char c);
+void	ft_showchartab(int x, int y, char **tab);
 
 int		main(void)
 {
 
-/* strsplit */
+/* strsplit
 	ft_putendl("--- strsplit? --- ");
 	ft_putendl(" ------------ wordtrim -------------- ");
 	ft_putendl(ft_wordtrim("pancake", 'c'));
-
-/* strtrim */
+*/
+/* strtrim 
 	ft_putendl("--- strtrim ? --- ");
 	ft_putendl(ft_strtrim("     \n  HELLOWORLD     \n"));
 	ft_putendl(ft_strtrim("\t\n  \tAAA \t BBB\t\n  \t"));
 	ft_putendl(ft_strtrim("\t\n\t  \t\t\n\t"));
+*/
 
+	ft_putendl("--- count words ---");
+	ft_putendl("salut*les*amis");
+	ft_putnbr(ft_countwords("salut*les*amis", '*'));
+	ft_putchar('\n');
+	ft_putendl("**salut***les*amis*");
+	ft_putnbr(ft_countwords("salut*les*amis", '*'));
+	ft_putchar('\n');
+	ft_putchar('\n');
+
+	ft_putendl("--- show char table ---");
+char tab[3][3];
+	tab[0][0] = 'a';
+	tab[0][1] = 'b';
+	tab[0][2] = 'c';
+	tab[1][0] = 'd';
+	tab[1][1] = 'e';
+	tab[1][2] = 'f';
+	tab[2][0] = 'g';
+	tab[2][1] = 'h';
+	tab[2][2] = 'j';
+/*
+	char	tab[][] = {{'a', 'b', 'c'},{'d', 'e', 'f'},{'g', 'h', 'i'}};*/
+	ft_showchartab(3, 3, tab);
 	return (0);
 }
