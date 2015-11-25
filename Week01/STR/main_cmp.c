9ce91396826f81d2e03c9b063e55ce113cb69b30
Alice GRELE <agrele@e2r2p7.42.fr>
2015-11-25 20:13:50 +0100

    just keep swimming

--- Week01/STR/main_cmp.c
+++ Week01/STR/main_cmp.c
@@ -4,12 +4,13 @@
 
 int		main(void)
 {
-/* strcmp */
-	ft_putendl("--- strcmp? --- ");
 	char s1[] = "apples";
 	char s2[] = "watermelons";
 	char s3[] = "roadkill";
 	char s4[] = "road";
+
+/* strcmp */
+	ft_putendl("--- strcmp? --- ");
 	printf("%s > %s ? \n", s1, s1);
 	ft_putnbr(ft_strcmp(s1, s1));
 	ft_putchar('\n');
@@ -49,6 +50,47 @@ int		main(void)
 	printf("%s > %s ? \n", "\0", "k");
 	ft_putnbr(strcmp("\0", "k"));
 	ft_putchar('\n');
+
+/* strncmp */
+	ft_putendl("--- strncmp? --- ");
+	printf("%s > %s ? n = 3\n", s1, s1);
+	ft_putnbr(ft_strncmp(s1, s1, 3));
+	ft_putchar('\n');
+	printf("%s > %s ? \n", s1, s2);
+	ft_putnbr(ft_strncmp(s1, s2, 1));
+	ft_putchar('\n');
+	printf("%s > %s ? \n", s2, s1);
+	ft_putnbr(ft_strncmp(s2, s1, 1));
+	ft_putchar('\n');
+	printf("%s > %s ? \n", s3, s4);
+	ft_putnbr(ft_strncmp(s3, s4, 1));
+	ft_putchar('\n');
+	printf("%s > %s ? \n", s4, s3);
+	ft_putnbr(ft_strncmp(s4, s3, 1));
+	ft_putchar('\n');
+	printf("%s > %s ? \n", "\0", "k");
+	ft_putnbr(ft_strncmp("\0", "k", 1));
+	ft_putchar('\n');
+	ft_putchar('\n');
+
+	ft_putendl("Real strncmp test : ");
+	printf("%s > %s ? \n", s1, s1);
+	ft_putnbr(strncmp(s1, s1, 1));
+	ft_putchar('\n');
+	printf("%s > %s ? \n", s1, s2);
+	ft_putnbr(strncmp(s1, s2, 1));
+	ft_putchar('\n');
+	printf("%s > %s ? \n", s2, s1);
+	ft_putnbr(strncmp(s2, s1, 1));
+	ft_putchar('\n');
+	printf("%s > %s ? \n", s3, s4);
+	ft_putnbr(strncmp(s3, s4, 1));
+	ft_putchar('\n');
+	printf("%s > %s ? \n", s4, s3);
+	ft_putnbr(strncmp(s4, s3, 1));
+	ft_putchar('\n');
+	printf("%s > %s ? \n", "\0", "k");
+	ft_putnbr(strncmp("\0", "k", 1));
 
 	return (0);
 }
--- Week01/STR/test_cmp.sh
+++ Week01/STR/test_cmp.sh
@@ -1,2 +1,2 @@
-clear && gcc -Wall -Wextra -Werror ft* main4.c ../WRITE/ft* ../EXTRA/ft* && ./a.out
+clear && gcc -Wall -Wextra -Werror ft* main_cmp.c ../WRITE/ft* ../EXTRA/ft* && ./a.out
 rm a.out
