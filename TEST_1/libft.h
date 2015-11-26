89fe70deaf27e799a841f5b6ffdbfb159c1caabb
Alice GRELE <agrele@e2r1p2.42.fr>
2015-11-26 13:48:01 +0100

    added test folder

---
+++ TEST_1/libft.h
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agrele <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/23 18:38:31 by agrele            #+#    #+#             */
+/*   Updated: 2015/11/26 11:56:37 by agrele           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <string.h>
+# define TRUE 1
+# define FALSE 0
+
+/* idiot 
+# define HEADER "../libft.h"
+*/
+
+/* NORMINETTE OK */
+
+/* PUT */
+void	ft_putchar(char c);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr(char const *s);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putnbr(int n);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putendl(char const *s);
+void	ft_putendl_fd(char const *s, int fd);
+
+/* IS */
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+
+/* STR */
+
+/* NEW */
+int		ft_strlen(const char *s);
+char	*ft_strcpy(char *restrict dst, const char *restrict src);
+char	*ft_strncpy(char *restrict dst, const char *restrict src, size_t n);
+void	ft_strclr(char *s);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strstr(const char *s1, const char *s2);
+char	*ft_strnstr(const char *s1, const char *s2, size_t n);
+
+/* EXTRA */
+int		ft_ispos(int n);
+int		ft_isneg(int n);
+char	*ft_putsign(int n);
+void	ft_swap(int *a, int *b);
+
+#endif 
